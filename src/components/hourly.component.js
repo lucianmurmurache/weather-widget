e4a1f84853583b9d6202eb7d6ae1a942ac6e6e1d
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-26 15:37:37 +0200

    Added hourly component and styling

---
+++ src/components/hourly.component.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './hourly.component.css';
+import { formatTime } from '../utilities';
+import { Temp } from './temp.component';
+import { WeatherIcon } from './weather-icon.component';
+
+function Hourly({ hourly }) {
+    return (
+        <div className='hourly'>
+            {hourly.map(hour => (
+                <div className='hour'>
+                    <div className='hour-time'>{formatTime(hour.observation_time.value)}</div>
+                    <div className='hour-icon'><WeatherIcon value={hour.weather_code.value} /></div>
+                    <div className='hour-temp'><Temp value={hour.temp.value} />°</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export { Hourly };
