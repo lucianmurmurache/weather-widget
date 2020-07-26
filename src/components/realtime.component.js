f7168d77c20009f7891e5c99381b177530ee1499
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-07-26 15:38:14 +0200

    Added realtime component and styling

---
+++ src/components/realtime.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './realtime.component.css';
+import { Temp } from './temp.component';
+import { WeatherIcon } from './weather-icon.component';
+import { prettyPrintWeatherCode } from '../utilities';
+
+function Realtime({ realtime }) {
+    return (
+        <div className='realtime'>
+            <div className='realtime-temp'><Temp value={realtime.temp.value} /></div>
+            <div className='realtime-temp-degrees'>°C</div>
+            <div>
+                <div className='realtime-weather-code'>{prettyPrintWeatherCode(realtime.weather_code.value)}</div>
+                <div className='realtime-feels-like'>Feels Like <Temp value={realtime.feels_like.value} />°</div>
+            </div>
+            <div className='realtime-icon'>
+                <WeatherIcon value={realtime.weather_code.value} />
+            </div>
+        </div>
+    );
+};
+
+export { Realtime };
